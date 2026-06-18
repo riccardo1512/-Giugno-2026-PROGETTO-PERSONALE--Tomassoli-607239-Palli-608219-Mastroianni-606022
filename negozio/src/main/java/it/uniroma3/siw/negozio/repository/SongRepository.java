6cb53234b9833d8934105259023facec1a1122c7
ElenaPalli <204370874+ElenaPalli@users.noreply.github.com>
2026-06-18 18:33:27 +0200

    chore: remove deprecated file utility module

--- negozio/src/main/java/it/uniroma3/siw/negozio/repository/SongRepository.java
+++
@@ -1,8 +0,0 @@
-package it.uniroma3.siw.negozio.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import it.uniroma3.siw.negozio.model.Song;
-
-public interface SongRepository extends CrudRepository<Song, Long> {
-
-}
