d71a6e8c8c05a001a662fd44b28bb10b2240bc3d
Riccardo <111280428+riccardo1512@users.noreply.github.com>
2026-06-18 13:26:15 +0200

    first commit

---
+++ negozio/src/main/java/it/uniroma3/siw/negozio/NegozioApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.negozio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegozioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegozioApplication.class, args);
+	}
+
+}
