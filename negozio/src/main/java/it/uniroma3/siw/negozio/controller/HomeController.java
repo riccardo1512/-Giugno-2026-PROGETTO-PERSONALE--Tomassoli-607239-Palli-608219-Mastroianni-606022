7d0a5825a0704a47ecaa2a62be01a9e9ccca8145
Riccardo <111280428+riccardo1512@users.noreply.github.com>
2026-06-22 00:10:08 +0200

    feat(views): add cd and homepage views, fix oauth configuration

--- negozio/src/main/java/it/uniroma3/siw/negozio/controller/HomeController.java
+++ negozio/src/main/java/it/uniroma3/siw/negozio/controller/HomeController.java
@@ -3,11 +3,21 @@ package it.uniroma3.siw.negozio.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+import it.uniroma3.siw.negozio.service.CDService;
+
 @Controller
 public class HomeController {
 
+    private final CDService cdService;
+
+    public HomeController(CDService cdService) {
+        this.cdService = cdService;
+    }
+
     @GetMapping("/")
-    public String getHome() {
+    public String getHome(Model model) {
+        model.addAttribute("cds", cdService.findAll());
         return "index";
     }
 }
